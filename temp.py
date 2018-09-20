ab25ae16167696fbd5893f927b31a7fcb822fb9d
vbscript055246 <vbscript055246@gmail.com>
2018-09-21 00:17:35 +0800

    better~

--- temp.py
+++ temp.py
@@ -1,11 +1,16 @@
 import datetime
 
+
+def get_month(num):
+    return num if num > 0 else (12 + num)
+
+
 print("Hello~")
 BDY = input("Birthday:year")
 BDM = input("Birthday:month")
 BDD = input("Birthday:date")
-today = datetime.date.today().year
+today = datetime.date.today()
+print(str(today.year - int(BDY)) + " years old")
 
-print(str(today - int(BDY)) + " years old")
-print(datetime.date(int(BDY), int(BDM), int(BDD)).month)
+print(get_month(datetime.date(int(BDY), int(BDM), int(BDD)).month - today.month))
 
