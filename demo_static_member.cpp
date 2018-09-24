710d17a021d52f069cd3b5aaad1707e49806f874
vbscript055246 <vbscript055246@gmail.com>
2018-09-24 10:58:05 +0800

    little update~

--- demo_static_member.cpp
+++ demo_static_member.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-class node{
+class node{ //有使用static member
 	public:
-		static int count;
-		node(){		//建構函式 
+		static int count; 
+		node(){		//建構式
 			count++;
 		}
 			
@@ -12,29 +12,31 @@ class node{
 int node::count = 0; //static member initialize
 
 
-class Node{
+class Node{ //沒有使用static member
 	public:
-		int count; 	//建構函式 
-		Node():count(0){
+		int count; 	
+		Node():count(0){ //建構式
 			count++;
 		}
 };
 
 
 int main(){
-	
+	//有使用static member
 	node a;
 	node b;
 	cout << a.count << "\n";
 	a.count = 1;
 	b.count = 0;
 	cout << a.count << "\n";
+	//大家的count是同一個
 	
-	
+	//沒有使用static member
 	Node A;
 	Node B;
 	cout << A.count << "\n";
 	A.count = 1;
 	B.count = 0;
 	cout << A.count << "\n";
+	//大家的count是不同的
 } 
