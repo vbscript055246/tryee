17b10f39616bcbe73f678ccfdb97dcb9e7df7fae
vbscript055246 <vbscript055246@gmail.com>
2018-09-29 00:19:03 +0800

    HW 9.26-2 update~

--- homework9.26-2.py
+++ homework9.26-2.py
@@ -1,14 +1,17 @@
 # 複雜度O(n)
 def transpose(array):
     ans = []
-    for i in range(len(array)):
-        ans.append(list(range(len(array[0]))))
+    for i in range(len(array[0])):
+        ans.append([0]*len(array))
+
     for i, col in enumerate(array):
         for j, row in enumerate(col):
             ans[j][i] = row
     return ans
 
 
-data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+data = [[1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]]
 print(transpose(data))
 
