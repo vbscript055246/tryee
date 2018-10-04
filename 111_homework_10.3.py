1f2b890d35a3605767670a96dcad28b7a7017f6b
vbscript055246 <vbscript055246@gmail.com>
2018-10-04 18:40:04 +0800

    HW update~

--- 111_homework_10.3.py
+++ 111_homework_10.3.py
@@ -1,5 +1,6 @@
-a = int(input("請輸入:\n"))
-while a != 1:a = (3 * a + 1) if (a % 2) else a // 2;print(a)
+def f(x):
+    while x != 1:x = (3 * x + 1) if (x % 2) else x // 2;print(x)
+f(int(input("請輸入:\n")))
 
 
 # ======想覺得有趣的話看看你家電腦可以算到多少XD~
@@ -8,7 +9,7 @@ while a != 1:a = (3 * a + 1) if (a % 2) else a // 2;print(a)
 #try:
 #    while 1:
 #        a = i
-#        while a != 1: a = (3 * a + 1) if (a % 2) else a // 2
+#       while a != 1: a = (3 * a + 1) if (a % 2) else a // 2
 #        i += 1
 #        print(i)
 #except:
