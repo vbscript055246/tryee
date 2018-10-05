6058157186d99008b55a55c52ac60fcef6beed6c
vbscript055246 <vbscript055246@gmail.com>
2018-10-05 13:36:11 +0800

    HW update~

--- 111_homework_10.3.py
+++ 111_homework_10.3.py
@@ -1,5 +1,5 @@
 def f(x):
-    while x != 1:x = (3 * x + 1) if (x % 2) else x // 2;print(x)
+    while x != 1:x = (3 * x + 1) if (x & 1) else x // 2;print(x)
 f(int(input("請輸入:\n")))
 
 
@@ -9,7 +9,7 @@ f(int(input("請輸入:\n")))
 #try:
 #    while 1:
 #        a = i
-#       while a != 1: a = (3 * a + 1) if (a % 2) else a // 2
+#       while a != 1: a = (3 * a + 1) if (a & 1) else a // 2
 #        i += 1
 #        print(i)
 #except:
